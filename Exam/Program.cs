0be6e7e4e201291d1ca57ad7ff0fccfe553ce4bd
LUIS AMIR AQUINO MEJÍA <la2023-0685@uce.edu.do>
2024-07-12 16:18:55 -0400

    Agregar archivos de proyecto.

---
+++ Exam/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class MaterialBibliografico
+{
+
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+
+ 
+    public MaterialBibliografico(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+    }
+   public void MostrarInformacion()
+    {
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MaterialBibliografico libro = new MaterialBibliografico("El examen", "Luis Aquino");
+
+       
+        libro.MostrarInformacion();
+    }
+}
